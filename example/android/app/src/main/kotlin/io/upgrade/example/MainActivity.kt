35132ee1c0c211abb6b84a1bdfa46ee7f2a7944e
LiWeNHuI <sdgrlwh@163.com>
2022-01-14 15:27:58 +0800

    🎉 Initial commit.

---
+++ example/android/app/src/main/kotlin/io/upgrade/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.upgrade.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ test/upgrade_util_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:upgrade_util/upgrade_util.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('upgrade_util');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await UpgradeUtil.platformVersion, '42');
+  });
+}

707d2926041efe1f16452b90ecde0330884cb756
LiWeNHuI <sdgrlwh@163.com>
2024-02-23 19:12:35 +0800

    🎉发布版本 - 2.3.5

--- test/upgrade_util_test.dart
+++ test/upgrade_util_test.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('upgrade_util');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {});
